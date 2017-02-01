e1d52e5a130c5a9773ee1d5e0a2c883dbbdbf2d9
dougfort <doug.fort@gmail.com>
2017-02-01 16:14:47 -0500

    remove the types that got moves to the gocards repository

--- types/types.go
+++ types/types.go
@@ -1,51 +1,13 @@
 package types
 
-// Suit represents the suit of a card
-type Suit uint8
-
-// Suit values
-const (
-	NullSuit Suit = iota
-	Clubs
-	Diamonds
-	Hearts
-	Spades
+import (
+	"github.com/dougfort/gocards"
 )
-
-// Rank represents the order of a Suit
-type Rank uint8
-
-// Rank Values
-const (
-	NullRank Rank = iota
-	Ace
-	Two
-	Three
-	Four
-	Five
-	Six
-	Seven
-	Eight
-	Nine
-	Ten
-	Jack
-	Queen
-	King
-)
-
-// CardType represents the value of a single card
-type CardType struct {
-	Suit Suit
-	Rank Rank
-}
-
-// CardsType is a group of Card(s)
-type CardsType []CardType
 
 // StackType reresesnts on stack of cards in the Tableau
 type StackType struct {
 	HiddenCount int
-	Cards       CardsType
+	Cards       gocards.Cards
 }
 
 // TableauWidth is the number of stacks in the Tableau
