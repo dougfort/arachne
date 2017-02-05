1e50014e929ce23a33af6196f986566ee7d5672e
dougfort <doug.fort@gmail.com>
2017-02-05 16:33:51 -0500

    use gorilla mux to start defining REST interface

--- arachneserv/main.go
+++ arachneserv/main.go
@@ -3,14 +3,22 @@ package main
 import (
 	"io"
 	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
-func hello(w http.ResponseWriter, r *http.Request) {
+const gamesPath = "/arachne/games/"
+
+func newGameHandler(w http.ResponseWriter, r *http.Request) {
 	io.WriteString(w, "Hello world!")
 }
 
 func main() {
-	mux := http.NewServeMux()
-	mux.HandleFunc("/", hello)
-	http.ListenAndServe(":8000", mux)
+	r := mux.NewRouter()
+
+	// REST: Create a new entry in the collection. The new entry's URI is assigned
+	// automatically and is usually returned by the operation.
+	r.HandleFunc(gamesPath, newGameHandler).Methods("POST")
+
+	http.ListenAndServe(":8000", r)
 }
