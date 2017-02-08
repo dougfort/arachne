dd29ead641df54394106d4cb1558dad4dd791cf2
dougfort <doug.fort@gmail.com>
2017-02-08 12:52:36 -0500

    get the game directly from game
    we'll worry about communicating with the server later

--- textclient/new_game.go
+++ textclient/new_game.go
@@ -1,28 +1,12 @@
 package main
 
 import (
-	"fmt"
-	"io/ioutil"
-	"log"
-	"net/http"
+	"github.com/dougfort/arachne/game"
 )
 
-const method = "http"
-const host = "localhost"
-const port = 8000
-const gamesPath = "arachne/games/"
-
 func newGame() (gameData, error) {
-	uri := fmt.Sprintf("%s://%s:%d/%s", method, host, port, gamesPath)
-	resp, err := http.Post(uri, "application/octet-stream", nil)
-	if err != nil {
-		return gameData{}, fmt.Errorf("http POST failed: %s", err)
-	}
-	x, err := ioutil.ReadAll(resp.Body)
-	resp.Body.Close()
-	if err != nil {
-		return gameData{}, fmt.Errorf("read body failed: %s", err)
-	}
-	log.Printf("debug: newGame: body = %s", string(x))
-	return gameData{}, fmt.Errorf("not implemented")
+	var g gameData
+
+	g.remote = game.New()
+	return g, nil
 }
