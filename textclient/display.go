bec28d4de4d258ef8843b8273d2197a72cb17efd
dougfort <doug.fort@gmail.com>
2017-02-17 20:45:25 -0500

    move card rune images to gocards/standard

--- textclient/display.go
+++ textclient/display.go
@@ -3,72 +3,9 @@ package main
 import (
 	"fmt"
 
-	"github.com/dougfort/gocards"
 	"github.com/dougfort/gocards/standard"
 
 	"github.com/dougfort/arachne/game"
-)
-
-const displayCardBack = '🂠'
-
-// displayCardsMap maps cards to unicode images
-type displayCardsMap map[gocards.Card]rune
-
-var (
-	displayCards = displayCardsMap{
-		gocards.Card{Suit: standard.Clubs, Rank: standard.Ace}:      '🃑',
-		gocards.Card{Suit: standard.Clubs, Rank: standard.Two}:      '🃒',
-		gocards.Card{Suit: standard.Clubs, Rank: standard.Three}:    '🃓',
-		gocards.Card{Suit: standard.Clubs, Rank: standard.Four}:     '🃔',
-		gocards.Card{Suit: standard.Clubs, Rank: standard.Five}:     '🃕',
-		gocards.Card{Suit: standard.Clubs, Rank: standard.Six}:      '🃖',
-		gocards.Card{Suit: standard.Clubs, Rank: standard.Seven}:    '🃗',
-		gocards.Card{Suit: standard.Clubs, Rank: standard.Eight}:    '🃘',
-		gocards.Card{Suit: standard.Clubs, Rank: standard.Nine}:     '🃙',
-		gocards.Card{Suit: standard.Clubs, Rank: standard.Ten}:      '🃚',
-		gocards.Card{Suit: standard.Clubs, Rank: standard.Jack}:     '🃛',
-		gocards.Card{Suit: standard.Clubs, Rank: standard.Queen}:    '🃝',
-		gocards.Card{Suit: standard.Clubs, Rank: standard.King}:     '🃞',
-		gocards.Card{Suit: standard.Diamonds, Rank: standard.Ace}:   '🃁',
-		gocards.Card{Suit: standard.Diamonds, Rank: standard.Two}:   '🃂',
-		gocards.Card{Suit: standard.Diamonds, Rank: standard.Three}: '🃃',
-		gocards.Card{Suit: standard.Diamonds, Rank: standard.Four}:  '🃄',
-		gocards.Card{Suit: standard.Diamonds, Rank: standard.Five}:  '🃅',
-		gocards.Card{Suit: standard.Diamonds, Rank: standard.Six}:   '🃆',
-		gocards.Card{Suit: standard.Diamonds, Rank: standard.Seven}: '🃇',
-		gocards.Card{Suit: standard.Diamonds, Rank: standard.Eight}: '🃈',
-		gocards.Card{Suit: standard.Diamonds, Rank: standard.Nine}:  '🃉',
-		gocards.Card{Suit: standard.Diamonds, Rank: standard.Ten}:   '🃊',
-		gocards.Card{Suit: standard.Diamonds, Rank: standard.Jack}:  '🃋',
-		gocards.Card{Suit: standard.Diamonds, Rank: standard.Queen}: '🃍',
-		gocards.Card{Suit: standard.Diamonds, Rank: standard.King}:  '🃎',
-		gocards.Card{Suit: standard.Hearts, Rank: standard.Ace}:     '🂱',
-		gocards.Card{Suit: standard.Hearts, Rank: standard.Two}:     '🂲',
-		gocards.Card{Suit: standard.Hearts, Rank: standard.Three}:   '🂳',
-		gocards.Card{Suit: standard.Hearts, Rank: standard.Four}:    '🂴',
-		gocards.Card{Suit: standard.Hearts, Rank: standard.Five}:    '🂵',
-		gocards.Card{Suit: standard.Hearts, Rank: standard.Six}:     '🂶',
-		gocards.Card{Suit: standard.Hearts, Rank: standard.Seven}:   '🂷',
-		gocards.Card{Suit: standard.Hearts, Rank: standard.Eight}:   '🂸',
-		gocards.Card{Suit: standard.Hearts, Rank: standard.Nine}:    '🂹',
-		gocards.Card{Suit: standard.Hearts, Rank: standard.Ten}:     '🂺',
-		gocards.Card{Suit: standard.Hearts, Rank: standard.Jack}:    '🂻',
-		gocards.Card{Suit: standard.Hearts, Rank: standard.Queen}:   '🂽',
-		gocards.Card{Suit: standard.Hearts, Rank: standard.King}:    '🂾',
-		gocards.Card{Suit: standard.Spades, Rank: standard.Ace}:     '🂡',
-		gocards.Card{Suit: standard.Spades, Rank: standard.Two}:     '🂢',
-		gocards.Card{Suit: standard.Spades, Rank: standard.Three}:   '🂣',
-		gocards.Card{Suit: standard.Spades, Rank: standard.Four}:    '🂤',
-		gocards.Card{Suit: standard.Spades, Rank: standard.Five}:    '🂥',
-		gocards.Card{Suit: standard.Spades, Rank: standard.Six}:     '🂦',
-		gocards.Card{Suit: standard.Spades, Rank: standard.Seven}:   '🂧',
-		gocards.Card{Suit: standard.Spades, Rank: standard.Eight}:   '🂨',
-		gocards.Card{Suit: standard.Spades, Rank: standard.Nine}:    '🂩',
-		gocards.Card{Suit: standard.Spades, Rank: standard.Ten}:     '🂪',
-		gocards.Card{Suit: standard.Spades, Rank: standard.Jack}:    '🂫',
-		gocards.Card{Suit: standard.Spades, Rank: standard.Queen}:   '🂭',
-		gocards.Card{Suit: standard.Spades, Rank: standard.King}:    '🂮',
-	}
 )
 
 func displayTableau(g gameData) {
@@ -80,12 +17,12 @@ ROW_LOOP:
 		var found bool
 		for col := 0; col < game.TableauWidth; col++ {
 			if row < g.remote.Tableau[col].HiddenCount {
-				image[col] = displayCardBack
+				image[col] = standard.RuneBack
 				found = true
 			} else {
 				visibleRow := row - g.remote.Tableau[col].HiddenCount
 				if visibleRow < len(g.remote.Tableau[col].Cards) {
-					image[col] = displayCards[g.remote.Tableau[col].Cards[visibleRow]]
+					image[col] = standard.Runes[g.remote.Tableau[col].Cards[visibleRow]]
 					found = true
 				} else {
 					image[col] = ' '
