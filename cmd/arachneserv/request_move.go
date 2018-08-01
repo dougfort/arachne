240cbb00f0e9e0462e9a4e2e90d57713a1c8ded7
dougfort <doug.fort@gmail.com>
2018-08-01 16:34:03 -0400

    remove global state (#3)
    keep everything for an individual game inside its stream loop

--- cmd/arachneserv/request_move.go
+++ cmd/arachneserv/request_move.go
@@ -9,39 +9,18 @@ import (
 )
 
 // RequestMove requests a move of cards
-func (s *arachneServer) RequestMove(
-	request *pb.MoveRequest,
-) (*pb.Game, error) {
+func requestMove(localGame *game.Game, request *pb.MoveRequest) (bool, error) {
 	var move game.MoveType
-	var localGame *game.Game
-	var pbGame pb.Game
 	var capture bool
-	var ok bool
 	var err error
 
 	move.FromCol = int(request.FromCol)
 	move.FromRow = int(request.FromRow)
 	move.ToCol = int(request.ToCol)
 
-	s.Lock()
-	defer s.Unlock()
-
-	if localGame, ok = s.active[request.GetId()]; !ok {
-		return nil, errors.Errorf("unknown game id %d", request.GetId())
-	}
-
 	if capture, err = localGame.Move(move); err != nil {
-		return nil, errors.Wrapf(err, "Move %s failed", move)
+		return false, errors.Wrapf(err, "Move %s failed", move)
 	}
 
-	pbGame.Seed = localGame.Deck.Seed()
-	pbGame.Stack = arachne2pb(localGame.Tableau)
-	pbGame.CardsRemaining = int32(localGame.Deck.RemainingCards())
-
-	if capture {
-		pbGame.CaptureCount++
-	}
-
-	s.active[pbGame.Id] = localGame
-	return &pbGame, nil
+	return capture, nil
 }
